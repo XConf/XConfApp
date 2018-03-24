ddb9f847c903cb5411cd4e323ac7e2114ebb587a
zetavg <mail@zeta.vg>
2018-03-24 19:06:23 +0800

    add yellow-box initializer to ignore some can't-fix warnings

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native'
 import { app } from './src/app.bs'
 
+import './initializers/yello-box'
+
 AppRegistry.registerComponent('XConf', () => app)
---
+++ initializers/yello-box.js
@@ -0,0 +1,9 @@
+/* eslint-disable quotes, max-len */
+import { YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings([
+  "Warning: componentWillMount is deprecated",
+  "Warning: componentWillUpdate is deprecated",
+  "Warning: componentWillReceiveProps is deprecated",
+  "Module RCTImageLoader requires main queue setup since it overrides `init` but doesn't implement `requiresMainQueueSetup`. In a future release React Native will default to initializing all native modules on a background thread unless explicitly opted-out of.",
+])
