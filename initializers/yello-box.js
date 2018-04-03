1a1bcd614849ca7912ea4ea44556cd650d9a8fad
zetavg <mail@zeta.vg>
2018-04-04 00:52:45 +0800

    suppress new warning: multiple renderers concurrently rendering the same context provider

--- initializers/yello-box.js
+++ initializers/yello-box.js
@@ -6,4 +6,5 @@ YellowBox.ignoreWarnings([
   "Warning: componentWillUpdate is deprecated",
   "Warning: componentWillReceiveProps is deprecated",
   "Module RCTImageLoader requires main queue setup since it overrides `init` but doesn't implement `requiresMainQueueSetup`. In a future release React Native will default to initializing all native modules on a background thread unless explicitly opted-out of.",
+  "Warning: Detected multiple renderers concurrently rendering the same context provider. This is currently unsupported.",
 ])
